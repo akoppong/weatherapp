5701955e9b1f45fa67336c95257d4a6ed1fdc311
akopong <kingkole>
2024-04-22 19:25:35 -0700

    Initial commit

---
+++ weatherapp/script.js
@@ -0,0 +1,28 @@
+let appID = '2d3745898c628a6b74224a44f0a04ae6';
+let units = 'imperial';
+let searchMethod = 'zip';
+
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else
+        searchMethod = 'q'; 
+}
+
+function searchWeather(searchTerm) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&APPID=${appID}&units=${units}`).then(result => {
+        return result.json();
+        }).then(result => {
+            init(result);
+        })
+    }
+
+    function init(resultFromServer) {
+        console.log(resultFromServer);
+    }
+
+    document.getElementById('searchBtn').addEventListener('click', () => {
+        let searchTerm = document.getElementById('searchInput').value;
+        if (searchTerm)
+            searchWeather(searchTerm);
+    })
