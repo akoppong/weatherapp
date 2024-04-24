486e388f75e93db49e482c645c16a178a6c7d2b4
akopong <kingkole>
2024-04-23 19:51:00 -0700

    Finished weatherapp

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -8,7 +8,7 @@ function getSearchMethod(searchTerm) {
     else
         searchMethod = 'q'; 
 }
-
+//call to the api and conver the response to json object
 function searchWeather(searchTerm) {
     getSearchMethod(searchTerm);
     fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appID}&units=${units}`).then(result => {
@@ -19,6 +19,7 @@ function searchWeather(searchTerm) {
     }
 
     function init(resultFromServer) {
+        //change the background color based on the weather response from the server
         switch(resultFromServer.weather[0].main) {
             case 'Clear':
                 document.body.style.backgroundImage = 'url("clear.jpg")';
@@ -44,16 +45,37 @@ function searchWeather(searchTerm) {
         }
 
         let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
-        let temperatureElement = document.getElementById('temeperature');
+        let temperatureElement = document.getElementById('temperature');
         let humudityElement = document.getElementById('humidity');
         let windSpeedElement = document.getElementById('windSpeed');
         let cityHeader = document.getElementById('cityHeader');
         let weatherIcon = document.getElementById('documentIconImg');
 
-        weatherIcon.src = 'https://openweathermap.org/img/wn/' + resultFromServer.weather[0].icon + '.png';
+        weatherIcon.src = 'https://openweathermap.org/img/wn/' + resultFromServer.weather[0].icon + '.png'; //getting an error here with the icon not loading
 
         console.log(resultFromServer);
+        
+        let resultDescription = resultFromServer.weather[0].description;
+        weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+        temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
+        windSpeedElement.innerHTML = 'Winds at ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
+        cityHeader.innerHTML = resultFromServer.name;
+        humudityElement.innerHTML = 'Humidity is at ' + resultFromServer.main.humidity + '%';
+
+        setPositionForWeatherInfo();
+
     }
+    function setPositionForWeatherInfo() {
+        let weatherContainer = document.getElementById('weatherContainer');
+        let weatherContainerHeight = weatherContainer.clientHeight;
+        let weatherContainerWidth = weatherContainer.clientWidth;
+
+        weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+        weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/2}px)`;
+        weatherContainer.style.visibility = 'visible';
+    }
+    
 
     document.getElementById('searchBtn').addEventListener('click', () => {
         let searchTerm = document.getElementById('searchInput').value;
